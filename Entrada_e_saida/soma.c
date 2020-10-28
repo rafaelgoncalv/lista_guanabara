aeb69227e4a11a585e36e7708e1ca1788da6175a
rafaelgoncalv <rafaeldocarmo1998s@gmail.com>
2020-10-28 18:13:36 -0300

    exercício 3

---
+++ Entrada_e_saida/soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int a,b,soma;
+
+    printf("Digite o valor de a e b:");
+    scanf("%d%d",&a,&b);
+
+    soma = a + b;
+
+    printf("%d + %d = %d",a,b,soma);
+
+
+    getchar();
+}
